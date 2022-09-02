352dc8ebddf0ba668b8a879810e2b8964b3d3638
solatjabeen <solatjabeen@yahoo.com>
2022-09-03 04:05:57 +0500

    Update explainableSTS.py

--- explainableSTS.py
+++ explainableSTS.py
@@ -2,10 +2,6 @@ import streamlit as st
 import docx2txt
 import streamlit.components.v1 as components
 
-# def load_image(image_file):
-# 	img = Image.open(image_file)
-# 	return img
-
 def main():
 	st.title("Explainable Semantic Text Similarity")
 	raw_text = st.text_area("Enter text here")
