87ba3d8512553334a46f1417d85aee092e0996c2
sumairasaeed <sumaira.syedsaeed@gmail.com>
2022-09-03 12:02:14 +0500

    Update explainableSTS.py

--- explainableSTS.py
+++ explainableSTS.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 
 def main():
 	st.title("Explainable Semantic Text Similarity")
-	raw_text = st.text_area("Enter text here")
+	raw_text = st.text_area("Enter text here please")
 	docx_file = st.file_uploader("Upload Document", type=["txt"])
 	
 
