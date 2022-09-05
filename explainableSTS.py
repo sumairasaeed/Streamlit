a609ee3d971df38492892465a7e13e0b797df04a
sumairasaeed <sumaira.syedsaeed@gmail.com>
2022-09-05 16:17:16 +0500

    Update explainableSTS.py

--- explainableSTS.py
+++ explainableSTS.py
@@ -5,6 +5,7 @@ import pickle
 from owlready2 import *
 from owlready2.pymedtermino2 import *
 from owlready2.pymedtermino2.umls import *
+from owlready2 import *
 
 def loadEntities():
 	#File containing mapped codes in textual form for 409 indus records
@@ -29,9 +30,12 @@ def main():
         #print(concept)
 	alltextEntitiesAllStr=loadEntities()
 	raw_text = st.text_area(str(alltextEntitiesAllStr[0:10]))
-	##raw_text = st.text_area("Enter text here please")
+	#raw_text = st.text_area("Enter text here please")
+	#default_world.set_backend(filename = "pym.sqlite3")
+	PYM = get_ontology("http://PYM/").load()
+	ICD10 = PYM["ICD10"]
 	docx_file = st.file_uploader("Upload Document", type=["txt"])
-	
+	raw_text = st.text_area(str(ICD10["E10"]))
 
 if __name__ == '__main__':
 	main()
