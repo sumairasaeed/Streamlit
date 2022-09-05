c1cdd1a4e401e5eedf67a6533668cb5dfa5c0667
sumairasaeed <sumaira.syedsaeed@gmail.com>
2022-09-05 18:09:37 +0500

    Update explainableSTS.py

--- explainableSTS.py
+++ explainableSTS.py
@@ -39,7 +39,19 @@ def main():
 	#PYM.load()
 	#ICD10 = PYM["ICD10"]
 	!wget https://medcat.rosalind.kcl.ac.uk/media/medmen_wstatus_2021_oct.zip -P ./data/
-	docx_file = st.file_uploader("Upload Document", type=["txt"])
+	vocab_file = st.file_uploader("Upload Document", type=[".dat"])
+	if vocab_file is not None:
+            path_in = vocab_file.name
+            print(path_in)
+        else:
+            path_in = None
+	
+	cdb_file = st.file_uploader("Upload Document", type=[".dat"])
+	if cdb_file is not None:
+            path_in_cdb_file = cdb_file.name
+            print(path_in_cdb_file)
+        else:
+            path_in = None
 	#raw_text = st.text_area(str(ICD10["E10"]))
 
 if __name__ == '__main__':
