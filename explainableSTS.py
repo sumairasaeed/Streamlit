f96121cb2765bb86c55bddb3ceb7e6a63b0a9ebe
sumairasaeed <sumaira.syedsaeed@gmail.com>
2022-09-07 12:29:47 +0500

    Update explainableSTS.py

--- explainableSTS.py
+++ explainableSTS.py
@@ -49,8 +49,18 @@ def main():
 	#site_url = 'https://ibacity-my.sharepoint.com/:u:/g/personal/sumairasaeed_iba_edu_pk/ERRCDIV60rJGozNFGi9_IgEBbZOXnuWFlcE8qMXD86gbrg'
 	site_url1='https://www.dropbox.com/s/be6fn4x21tvbdcn/medmen_wstatus_2021_oct.zip?dl=0'
 	site_url2='https://www.dropbox.com/s/oev6ut5879qbd68/mc_modelpack_snomed_int_3_feb_2022_a474096eb4566638.zip?dl=0'
-	file_name = wget.download(site_url)
+	##file_name = wget.download(site_url)
 	model_pack_path=file_name
+	
+	filename = site_url.split('/')[-1]
+
+	urllib.request.urlretrieve(url, file_name)
+	
+	# Writing the file to the local file system
+	with open(file_name,'wb') as output_file:
+	    output_file.write(req.content)
+	print('Downloading Completed')
+
 	print(file_name)
 	st.title(file_name)
 	#file_name='medmen_wstatus_2021_oct.zip'
