97bb96fc36e242e3f886cff34463615b66b3f4ff
sumairasaeed <sumaira.syedsaeed@gmail.com>
2022-09-07 11:24:55 +0500

    Update explainableSTS.py

--- explainableSTS.py
+++ explainableSTS.py
@@ -51,12 +51,12 @@ def main():
 	#site_url='https://www.dropbox.com/s/be6fn4x21tvbdcn/medmen_wstatus_2021_oct.zip?dl=0'
 	site_url='https://www.dropbox.com/s/oev6ut5879qbd68/mc_modelpack_snomed_int_3_feb_2022_a474096eb4566638.zip'
 	#?dl=0'
-	#file_name = wget.download(site_url)
-	#print(file_name)
+	file_name = wget.download(site_url)
+	print(file_name)
 	#st.title(file_name)
 	#file_name='medmen_wstatus_2021_oct.zip'
 	#status = requests.get(site_url)
-	urllib.request.urlretrieve(site_url, "./code.zip")
+	#urllib.request.urlretrieve(site_url, "./code.zip")
 	##status=urllib.request.urlretrieve(site_url, file_name)
 	##st.title(status)
 	#st.title(file_name)
@@ -65,8 +65,8 @@ def main():
 
 	# Download the model_pack from the models section in the github repo.
 	#cat = CAT.load_model_pack('/snomedsimple.zip')
-	cat = CAT.load_model_pack("./code.zip")
-	#cat = CAT.load_model_pack(file_name)
+	#cat = CAT.load_model_pack("./code.zip")
+	cat = CAT.load_model_pack(file_name)
 
 	# Test it
 	text = "My simple document with kidney failure and fever and cough and flue"
