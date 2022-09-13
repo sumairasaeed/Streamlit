f0f9aea9a7d91d70f182c6222b75796c7466c595
sumairasaeed <sumaira.syedsaeed@gmail.com>
2022-09-13 16:43:59 +0500

    Update explainableSTS.py

--- explainableSTS.py
+++ explainableSTS.py
@@ -17,9 +17,6 @@ def downloadFileGdrive():
         url = "https://drive.google.com/file/d/16MagXKJ40efUsoiL2FygIBvXIBHnM906/view?usp=sharing"
 	output = "medcatLarge1.zip"
 	file=gdown.download(url=url, output=output, quiet=False, fuzzy=True)
-        print(file)
-        #file=gdown.download(url, output, quiet=False)
-        #st.text_area(str(file))
         return output
 
 
